babee09227f77c286098d60d21092b28c58cd96c
antronic <antronic.inc@gmail.com>
2016-03-08 18:04:50 +0700

    edit content

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,7 +36,7 @@ app.controller('date', function($scope) {
 var item = {
   en: {
     fp: "<strong>TEDxKMUTT</strong> is a group of student who interested in creativity and idea exchange to  develop community." +
-      "We host TEDx events to share an intention for improving our society.",
+      " We host TEDx events to share an intention for improving our society.",
     sp: "The TEDx Program is designed to help communities, organizations and individuals  " +
       "to spark conversation and connection through local TED-like experiences.",
     tp: "Now, We need someone who are interested in TEDx to be a part of our team.  " + "We want individuals who have high potential, high responsibility, and positive passion.  " +
