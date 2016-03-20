4ec7513b5c9aaec22f94e34a785055a1a07598a7
KANISORN <firstziiz.k@gmail.com>
2016-03-20 23:19:03 +0700

    maybe front-end final

--- app/app.js
+++ app/app.js
@@ -18,6 +18,11 @@ app.config(function($routeProvider, $locationProvider){
     controller: 'aboutController',
     controllerAs: 'about'
   })
+  .when('/success/',{
+    templateUrl: "app/template/success.html",
+    controller: 'successController',
+    controllerAs: 'success'
+  })
   .otherwise({ redirectTo: '/'});
 
   // $locationProvider.html5Mode(true);
@@ -28,7 +33,7 @@ app.config(function($routeProvider, $locationProvider){
 app.run(function($rootScope, $location, $anchorScroll) {
   //when the route is changed scroll to the proper element.
   $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
-    if($location.hash()) $anchorScroll();  
+    if($location.hash()) $anchorScroll();
   });
 });
 
@@ -83,6 +88,31 @@ app.controller('aboutController',function($scope, $sce){
   };
 });
 
+app.controller('successController', function($scope, $sce) {
+  $scope.page = 'success';
+  $scope.content = hContent.en;
+  clang = 'en';
+  //
+  // $scope.lang = function(changeLang) {
+  //   if (changeLang == 'en') {
+  //     clang = 'en';
+  //     $scope.content = hContent.en;
+  //     // console.log("lang en");
+  //   } else {
+  //     clang = 'th';
+  //     $scope.content = hContent.th;
+  //     // console.log("lang th");
+  //   };
+  // };
+  // $scope.isLang = function(chk) {
+  //   // console.log(clang);
+  //   return clang == chk;
+  // };
+  $scope.getHTML = function(html){
+    return $sce.trustAsHtml(html);
+  };
+});
+
 app.controller('date', function($scope) {
   this.dates = new Date();
   $scope.year = this.dates.getFullYear();
@@ -120,8 +150,8 @@ app.controller('regController',['$scope', '$http', '$location','$anchorScroll',f
 	$scope.formData.creativeQ1;
 	$scope.formData.pmQ1;
 	$scope.formData.speaker;
-	
-	
+
+
 
 	//Is Submit button hit?
 	$scope.submitted = false;
@@ -129,7 +159,7 @@ app.controller('regController',['$scope', '$http', '$location','$anchorScroll',f
 
 	//Store sub case status
 	$scope.subCase = {};
-	
+
 	//Dropdown data
 	$scope.dropdown = {};
 
@@ -144,44 +174,44 @@ app.controller('regController',['$scope', '$http', '$location','$anchorScroll',f
 	$scope.dropdown.faculty.val = ["Eng","Sci","Ind","IT","Arch","FIBO","MultiSci"];
 	$scope.dropdown.faculty.select = null;
 	$scope.dropdown.faculty.text = "--";
-	
+
 	$scope.subCase.department = {};
 	$scope.subCase.department.select = null;
 	$scope.subCase.department.text = "--";
-	
+
 	$scope.dropdown.depEng = {};
 	$scope.dropdown.depEng.data = ["วิศวกรรมเคมี","วิศวกรรมเครื่องกล","วิศวกรรมไฟฟ้า","วิศวกรรมโยธา","วิศวกรรมอุตสาหการและเมคคาทรอนิกส์","วิศวกรรมคอมพิวเตอร์","วิศวกรรมระบบควบคุมและเครื่องมือวัด","วิศวกรรมสิ่งแวดล้อม","วิศวกรรมไฟฟ้าสื่อสารและอิเล็กทรอนิกส์","วิศวกรรมวัสดุและเครื่องมือ"];
 	$scope.dropdown.depEng.val = ["CHM","ME","EE","CE","PEMCE","CPE","INC","ENV","ENE","TME"];
 	$scope.dropdown.depEng.select = null;
 	$scope.dropdown.depEng.text = "--";
-	
+
 	$scope.dropdown.depSci = {};
 	$scope.dropdown.depSci.data = ["คณิตศาสตร์","เคมี","ฟิสิกส์ประยุกต์","จุลชีววิทยา","วิทยาการคอมพิวเตอร์ประยุกต์","สถิติ","วิทยาศาสตร์และเทคโนโลยีการอาหาร"];
 	$scope.dropdown.depSci.val = ["MTH","CHE","PHY","MIC","CS","STA","FS"];
 	$scope.dropdown.depSci.select = null;
-	
+
 	$scope.dropdown.depInd = {};
 	$scope.dropdown.depInd.data = ["เทคโนโลยีการพิมพ์และบรรจุภัณฑ์","เทคโนโลยีอุตสาหกรรม","วิทยาการคอมพิวเตอร์ประยุกต์-มัลติมีเดีย","วิศวกรรมเครื่องกล","วิศวกรรมไฟฟ้า","วิศวกรรมโยธา","วิศวกรรมอุตสาหการ","เทคโนโลยีการศึกษาและการสื่อสารมวลชน","เทคโนโลยีมีเดีย","มีเดียอาตส์","มีเดียทางการแพทย์และวิทยาศาสตร์"];
 	$scope.dropdown.depInd.val = ["PRI","InT","CM","ME","EE","CE","PE","EDU","MET","MEA","MES"];
 	$scope.dropdown.depInd.select = null;
 	$scope.dropdown.depInd.text = "--";
-	
+
 	$scope.dropdown.depIT = {};
 	$scope.dropdown.depIT.data = ["วิทยาการคอมพิวเตอร์","เทคโนโลยีสารสนเทศ"];
 	$scope.dropdown.depIT.val = ["CS","IT"];
 	$scope.dropdown.depIT.select = null;
-	
+
 	$scope.dropdown.depArch = {};
 	$scope.dropdown.depArch.data = ["สถาปัตยกรรม","สถาปัตยกรรมภายใน","การออกแบบอุตสาหกรรม","ออกแบบนิเทศศิลป์"];
 	$scope.dropdown.depArch.val = ["ARC","INT","IND","COM"];
 	$scope.dropdown.depArch.select = null;
-	
+
 	$scope.dropdown.depFIBO = {};
 	$scope.dropdown.depFIBO.data = ["วิศวกรรมหุ่นยนต์และระบบอัตโนมัติ"];
 	$scope.dropdown.depFIBO.val = ["RA"];
 	$scope.dropdown.depFIBO.select = null;
 	$scope.dropdown.depFIBO.text = "--";
-	
+
 	$scope.dropdown.depMultiSci = {};
 	$scope.dropdown.depMultiSci.data = ["วิทยาศาสตร์และเทคโนโลยี"];
 	$scope.dropdown.depMultiSci.val = ["ST"];
@@ -207,35 +237,35 @@ app.controller('regController',['$scope', '$http', '$location','$anchorScroll',f
 	$scope.checkDiv.group.val = ["PM","Cu","Cr"];
 	$scope.checkDiv.group.icon = ["briefcase","binoculars","paint-brush"];
 	$scope.checkDiv.group.select = null;
-	
+
 	$scope.subCase.subGroup = {};
 	$scope.subCase.subGroup.select;
 	$scope.subCase.subGroup.text;
 	$scope.subCase.subGroup.valid = true;
-	
+
 	$scope.checkDiv.subPM = {};
 	$scope.checkDiv.subPM.data = ["Sponsor","Finance","Support","Documentation","Contractor"];
 	$scope.checkDiv.subPM.val = ["Spo","Fin","Sup","Doc","Con"];
 	$scope.checkDiv.subPM.icon = ["bank","money","archive","book","edit"];
 	$scope.checkDiv.subPM.select = null;
-	
+
 	$scope.checkDiv.subCr = {};
 	$scope.checkDiv.subCr.data = ["Public Relation","Digital Production","Stage","Activity"];
 	$scope.checkDiv.subCr.val = ["PR","DP","Sta","Act"];
 	$scope.checkDiv.subCr.icon = ["comments-o","tv","bullhorn","users"];
 	$scope.checkDiv.subCr.select = null;
-	
+
 	$scope.subCase.subSubGroup = {};
 	$scope.subCase.subSubGroup.select;
 	$scope.subCase.subSubGroup.text;
 	$scope.subCase.subSubGroup.valid = true;
-	
+
 	$scope.checkDiv.subPR = {};
 	$scope.checkDiv.subPR.data = ["Creative","Marketing","Graphic Design","Copy Writing"];
 	$scope.checkDiv.subPR.val = ["Cre","Mar","GD","CW"];
 	$scope.checkDiv.subPR.icon = ["paper-plane","line-chart","photo","file-text-o"];
 	$scope.checkDiv.subPR.select = null;
-	
+
 	$scope.checkDiv.subDP = {};
 	$scope.checkDiv.subDP.data = ["Photographs","Video Recording","Process Monitoring"];
 	$scope.checkDiv.subDP.val = ["Pho","Vid","PMo"];
@@ -250,7 +280,7 @@ app.controller('regController',['$scope', '$http', '$location','$anchorScroll',f
 		$scope.formData[name] = $scope[type][name].val[index];
 		$scope.formData[name+'Full'] = $scope[type][name].text;
 		}
-	
+
 	$scope.selectFaculty = function(index)
 		{
 		$scope.dropdown.faculty.select = index;
@@ -259,25 +289,25 @@ app.controller('regController',['$scope', '$http', '$location','$anchorScroll',f
 		$scope.subCase.department.text = "--";
 		$scope.subCase.department.select = "dep"+$scope.dropdown.faculty.val[index];
 		$scope.formData.department = null;
-		
+
 		$scope.formData.facultyFull = $scope.dropdown.faculty.text;
 		$scope.formData.departmentFull = null;
 		}
-	
+
 	$scope.selectDepartment = function(index)
 		{
 		var depObj = $scope.subCase.department.select;
-		
+
 		$scope.dropdown[depObj].select = index;
 		$scope.formData.department = $scope.dropdown[depObj].val[index];
 		$scope.subCase.department.text = $scope.dropdown[depObj].data[index];
-		
+
 		$scope.formData.departmentFull = $scope.subCase.department.text;
 		}
-	
+
 	$scope.selectGroup = function(index)
 		{
-		
+
 		//clear old question if change group
 		if(index != $scope.checkDiv.group.select)
 			{
@@ -290,21 +320,21 @@ app.controller('regController',['$scope', '$http', '$location','$anchorScroll',f
 			$scope.regisForm.creativeQ1.$touched = false;
 			$scope.regisForm.pmQ1.$touched = false;
 			}
-		
+
 		//Clear other sub group if change group
 		$scope.checkDiv.subPM.select = null;
 		$scope.checkDiv.subCr.select = null;
 		$scope.checkDiv.subPR.select = null;
 		$scope.checkDiv.subDP.select = null;
-		
+
 		//Set select value
 		$scope.checkDiv.group.select = index;
 		$scope.checkDiv.group.text = $scope.checkDiv.group.data[index];
 		$scope.formData.group = $scope.checkDiv.group.val[index];
 		$scope.formData.groupFull = $scope.checkDiv.group.text;
-		
+
 		$scope.subCase.subGroup.select = "sub"+$scope.checkDiv.group.val[index];
-		
+
 		//Check if this group have sub group
 		if($scope.checkDiv.hasOwnProperty($scope.subCase.subGroup.select))
 			{
@@ -317,37 +347,37 @@ app.controller('regController',['$scope', '$http', '$location','$anchorScroll',f
 			$scope.subCase.subGroup.text = null;
 			$scope.subCase.subGroup.valid = true;
 			}
-		
+
 		//clear sub group data
 		$scope.formData.subGroup = null;
 		$scope.formData.subSubGroup = null;
 		$scope.formData.subGroupFull = null;
 		$scope.formData.subSubGroupFull = null;
-		
+
 		//clear sub sub group
 		$scope.subCase.subSubGroup.select = null;
 		$scope.subCase.subSubGroup.text = null;
 		$scope.subCase.subSubGroup.valid = true;
 		}
-	
+
 	$scope.selectSubGroup = function(index)
 		{
 		var subGroup = $scope.subCase.subGroup.select;
-		
+
 		//Clear other sub sub group if change sub group
 		$scope.checkDiv.subPR.select = null;
 		$scope.checkDiv.subDP.select = null;
-		
+
 		//Set select value
 		$scope.checkDiv[subGroup].select = index;
 		$scope.checkDiv[subGroup].text = $scope.checkDiv[subGroup].data[index];
 		$scope.formData.subGroup = $scope.checkDiv[subGroup].val[index];
 		$scope.formData.subGroupFull = $scope.checkDiv[subGroup].text;
-		
+
 		$scope.subCase.subGroup.valid = true;
-		
+
 		$scope.subCase.subSubGroup.select = "sub"+$scope.checkDiv[subGroup].val[index];
-		
+
 		//Check if this sub group have sub of sub group
 		if($scope.checkDiv.hasOwnProperty($scope.subCase.subSubGroup.select))
 			{
@@ -363,16 +393,16 @@ app.controller('regController',['$scope', '$http', '$location','$anchorScroll',f
 		$scope.formData.subSubGroup = null;
 		$scope.formData.subSubGroupFull = null;
 		}
-	
+
 	$scope.selectSubSubGroup = function(index)
 		{
 		var subSubGroup = $scope.subCase.subSubGroup.select;
-		
+
 		$scope.checkDiv[subSubGroup].select = index;
 		$scope.checkDiv[subSubGroup].text = $scope.checkDiv[subSubGroup].data[index];
 		$scope.formData.subSubGroup = $scope.checkDiv[subSubGroup].val[index];
 		$scope.formData.subSubGroupFull = $scope.checkDiv[subSubGroup].text;
-		
+
 		$scope.subCase.subSubGroup.valid = true;
 		}
 
@@ -380,20 +410,21 @@ app.controller('regController',['$scope', '$http', '$location','$anchorScroll',f
 		{
       	// console.log("TESt");
 		$scope.submitted = true;
-		
+
     	// console.log($scope.formData);
 		if($scope.regisForm.$valid && $scope.formData.faculty != null && $scope.formData.department != null && $scope.dropdown.year.select != null && $scope.checkDiv.gender.select != null && $scope.checkDiv.group.select != null && $scope.subCase.subGroup.valid && $scope.subCase.subSubGroup.valid && (($scope.checkDiv.group.select == 0 && $scope.formData.pmQ1 != null) || ($scope.checkDiv.group.select == 1 && $scope.formData.curatorQ1 != null && $scope.formData.curatorQ2 != null) || ($scope.checkDiv.group.select == 2 && $scope.formData.creativeQ1 != null)))
 			{
 			$scope.warning = false;
 			console.log($scope.formData);
 			//Sent to DB API
-      		return $http.post('/api/reg_sav', $scope.formData);
+  		$http.post('/api/reg_sav', $scope.formData);
+      $location.path('/success');
+
 			}
 		else
 			{
 			$scope.warning = true;
-			$location.hash('top');
-      		$anchorScroll();
+  		$anchorScroll();
 			}
 		}
 
